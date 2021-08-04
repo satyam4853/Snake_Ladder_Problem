9a1e435258796bb825bec2219ecd51e8c50e03b8
Your Name <you@example.com>
2021-08-04 15:43:30 +0530

    reactor: winning positon checked

--- Snake_Ladder_Problem/Snake_Ladder_Problem/Program.cs
+++ Snake_Ladder_Problem/Snake_Ladder_Problem/Program.cs
@@ -7,32 +7,35 @@ namespace Snake_Ladder_Problem
         static void Main(string[] args)
         {
             Console.WriteLine("This is a Snake and Ladder Problem");
-            int PlayerOne = 1;
-            int PlayerPosition = 0;
+            const int PlayerOne = 1;
+            const int PlayerPosition = 0;
             const int noplayoption = 0;
             const int ladderoption = 1;
             const int snakeoption = 2;
             int positionone = 0;
-            Random random = new Random();
-            int RollDie = random.Next(1, 7);
-            Console.WriteLine("Die is Rolls : " + RollDie);
-            int options = random.Next(0, 3);
-            switch (options)
+            while (positionone < 100)
             {
-                case noplayoption:
-                    positionone = positionone + 0;
-                    break;
-                case ladderoption:
-                    positionone = positionone + RollDie;
-                    break;
-                case snakeoption:
-                    positionone = positionone - RollDie;
-                    if (positionone < 0)
-                    {
-                        positionone = 0;
-                    }
-                    break;
+                Random random = new Random();
+                int dieRoll = random.Next(1, 7);
+                int options = random.Next(0, 3);
+                switch (options)
+                {
+                    case noplayoption:
+                        positionone = positionone + 0;
+                        break;
+                    case ladderoption:
+                        positionone = positionone + dieRoll;
+                        break;
+                    case snakeoption:
+                        positionone = positionone - dieRoll;
+                        if (positionone < 0)
+                        {
+                            positionone = 0;
+                        }
+                        break;
+                }
             }
+            Console.WriteLine("Player Wins With Position : " + positionone);
         }
     }
 }
