4e92cd3bdd4ea4ef9c6954a02b0c56cad9d8a181
Your Name <you@example.com>
2021-08-04 15:37:28 +0530

    refactor: checking for option

--- Snake_Ladder_Problem/Snake_Ladder_Problem/Program.cs
+++ Snake_Ladder_Problem/Snake_Ladder_Problem/Program.cs
@@ -6,12 +6,33 @@ namespace Snake_Ladder_Problem
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is snake and ladder problem");
+            Console.WriteLine("This is a Snake and Ladder Problem");
             int PlayerOne = 1;
             int PlayerPosition = 0;
+            const int noplayoption = 0;
+            const int ladderoption = 1;
+            const int snakeoption = 2;
+            int positionone = 0;
             Random random = new Random();
             int RollDie = random.Next(1, 7);
             Console.WriteLine("Die is Rolls : " + RollDie);
+            int options = random.Next(0, 3);
+            switch (options)
+            {
+                case noplayoption:
+                    positionone = positionone + 0;
+                    break;
+                case ladderoption:
+                    positionone = positionone + RollDie;
+                    break;
+                case snakeoption:
+                    positionone = positionone - RollDie;
+                    if (positionone < 0)
+                    {
+                        positionone = 0;
+                    }
+                    break;
+            }
         }
     }
 }
