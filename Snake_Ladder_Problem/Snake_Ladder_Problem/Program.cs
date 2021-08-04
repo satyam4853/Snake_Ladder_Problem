c938ea5ee60803b7a189dfa527e4d1ccd5e7b13c
Your Name <you@example.com>
2021-08-04 15:56:35 +0530

    refactor: game with two players

--- Snake_Ladder_Problem/Snake_Ladder_Problem/Program.cs
+++ Snake_Ladder_Problem/Snake_Ladder_Problem/Program.cs
@@ -7,43 +7,52 @@ namespace Snake_Ladder_Problem
         static void Main(string[] args)
         {
             Console.WriteLine("This is a Snake and Ladder Problem");
-            const int PlayerOne = 1;
-            const int PlayerPosition = 0;
             const int noplayoption = 0;
             const int ladderoption = 1;
             const int snakeoption = 2;
-            int positionone = 0;
+            int player = 0;
+            int[] playerposition = new int[2] { 0, 0 };
             int diceRollCount = 0;
-            while (positionone < 100)
+            while (playerposition[player] < 100)
             {
                 Random random = new Random();
                 int dieRoll = random.Next(1, 7);
                 diceRollCount++;
                 int options = random.Next(0, 3);
-                Console.WriteLine("Position: " + positionone);
                 switch (options)
                 {
                     case noplayoption:
-                        Console.WriteLine("Position: " + positionone);
+                        Console.WriteLine("Player " + player + "Position: " + playerposition[player]);
                         break;
                     case ladderoption:
-                        if (positionone + dieRoll <= 100)
+                        if (playerposition[player] + dieRoll <= 100)
                         {
-                            positionone = positionone + dieRoll;
+                            playerposition[player] = playerposition[player] + dieRoll;
                         }
-                        Console.WriteLine("Position: " + positionone);
+                        Console.WriteLine("Player " + player + "Position: " + playerposition[player]);
                         break;
                     case snakeoption:
-                        positionone = positionone - dieRoll;
-                        if (positionone < 0)
+                        playerposition[player] = playerposition[player] - dieRoll;
+                        if (playerposition[player] < 0)
                         {
-                            positionone = 0;
+                            playerposition[player] = 0;
                         }
-                        Console.WriteLine("Position: " + positionone);
+                        Console.WriteLine("Player " + player + " Position: " + playerposition[player]);
                         break;
                 }
+                if (options == noplayoption || options == snakeoption)
+                {
+                    if (player == 0)
+                    {
+                        player = 1;
+                    }
+                    else
+                    {
+                        player = 0;
+                    }
+                }
             }
-            Console.WriteLine("Player Wins With Position : " + diceRollCount);
+            Console.WriteLine("Player " + player + " Wins with Die Roll :" + diceRollCount);
         }
     }
 }
